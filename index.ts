21736b9f92b096fddce40e1603f19344b6654193
martinalbrnz <martinalbor96@gmail.com>
2022-08-18 23:05:35 -0300

    Add notes about mapped types

--- index.ts
+++ index.ts
@@ -5,9 +5,117 @@ let anotherVar: string // Explicit assignment
 let redundant: string = 'Hello world' // Not needed, avoid
 
 /* Custom types */
+// Used to declare data types
 type Style = 'bold' | 'italic' | 'underline' // Style could be any of this values
+type ComplexData = {
+	address: string
+	city?: string
+}
+// Note the use of ? for optional properties
+
+const dataType: ComplexData = {
+	address: 'Wallaby St. 42'
+}
+
+// We can set a type to readonly (or a specific field)
+type JustReading = {
+	readonly surname: 'Alerce'
+	readonly address: 'Alameda St 22'
+}
+
+// Mapped types
+// This is a mapped type, is used to convert one type into another
+type JustReadingAgain = Readonly<{
+	id: number
+	name: string
+}>
+
+// Also We can convert an existent type
+type ConvertedToReadonly = Readonly<ComplexData>
+
+// There are other built-in mapped types
+// Options flag
+type OptionsFlag<Type> = {
+	[Property in keyof Type]: boolean
+}
+
+type FeatureFlags = {
+	darkMode: () => void
+	newProfile: () => void
+}
+
+type FeatureOptions = OptionsFlag<FeatureFlags>
+// In this case OptionsFlag take the properties from FeatureFlags and transform it in boolean
+/* Equivalent expression
+type FeatureFlags = {
+	darkMode: () => boolean;
+	newProfile: () => boolean;
+}
+*/
+
+// There is a way to add or remove the readonly modifier
+type LockedType = {
+	readonly description: string
+	readonly isActive: boolean
+}
+
+type CreateMutable<Type> = {
+	-readonly  [Property in keyof Type]: Type[Property]
+}
+
+type UnlockedType = CreateMutable<LockedType>
+/* Equivalent
+type UnlockedType = {
+  description: string;
+  isActive: boolean;
+}
+*/
+
+type CreateLocked<Type> = {
+	+readonly [Property in keyof Type]: Type[Property]
+}
+
+type AnotherLockedType = CreateLocked<UnlockedType>
+/* Equivalent
+type AnotherLockedType = {
+  readonly description: string;
+  readonly isActive: boolean;
+}
+*/
+
+// It works with optional parameters too
+type NonOptionals = {
+	name: string
+	hasCar: boolean
+}
+
+type CreateOptionals<Type> = {
+	[Property in keyof Type]+?: Type[Property]
+}
+
+type OptionalsType = CreateOptionals<NonOptionals>
+/* Equivalent
+type OptionalsType = {
+  name?: string;
+  hasCar?: boolean;
+}
+*/
+
+type CreateNonOptionals<Type> = {
+	[Property in keyof Type]-?: Type[Property]
+}
+
+type NonOptionalsType = CreateNonOptionals<OptionalsType>
+/* Equivalent
+type NonOptionalsType = {
+  name: string;
+  hasCar: boolean;
+}
+*/
 
 /* Interfaces */
+// They are a way to define entities, to use with a derived class
+// It could contain methods, properties and events to describe data shapes
 interface Animal {
 	name: string,
 	legs: number,
@@ -44,6 +152,7 @@ const vehicle1: Vehicle = {
 	brand: 'bmw'
 }
 
+// Or not...
 const vehicle2: Vehicle = {
 	name: 'Cycle',
 	wheels: 2,
